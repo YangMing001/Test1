e00bfafb6b7a1b933805d19960a6b761c7fa07e8
YM <yangming@1yd.me>
2016-06-08 14:15:29 +0800

    helper

---
+++ Example/Classes/Test1Helper.h
@@ -0,0 +1,15 @@
+//
+//  Test1Helper.h
+//  Test1
+//
+//  Created by YM on 16/6/8.
+//  Copyright © 2016年 YM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test1Helper : NSObject
+
++ (void)log;
+
+@end
---
+++ Example/Classes/Test1Helper.m
@@ -0,0 +1,17 @@
+//
+//  Test1Helper.m
+//  Test1
+//
+//  Created by YM on 16/6/8.
+//  Copyright © 2016年 YM. All rights reserved.
+//
+
+#import "Test1Helper.h"
+
+@implementation Test1Helper
+
++ (void)log{
+    NSLog(@"Pod success");
+}
+
+@end
